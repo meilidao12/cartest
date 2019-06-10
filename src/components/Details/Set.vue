483a7804e71c754851a5925a2f2c7de897f900eb
meilidao12 <330629898@qq.com>
2019-06-10 16:10:44 +0800

    132

--- src/components/Details/Set.vue
+++ src/components/Details/Set.vue
@@ -1,14 +1,32 @@
 <template>
     <div>
-        <el-button @click="AddData()">添加</el-button>
+        <el-button @click="getData()">添加</el-button>
     </div>
 </template>
 <script>
 export default {
+    data(){
+        return{
+            list:[]
+        }
+    },
     methods:{
         AddData(){
             alert("数据添加");
+        },
+        getData(){
+            //请求数据
+            var api='http://api.map.baidu.com/telematics/v3/weather?location=%E5%98%89%E5%85%B4&output=json&ak=5slgyqGDENN7Sy7pw29IUvrZ"';
+            this.$http.get(api).then((response)=>{
+                console.log(response);
+                //注意this指向
+            },function(err){
+                console.log(err);
+            })
         }
+    },
+    mounted(){
+        this.getData();
     }
 }
 </script>
